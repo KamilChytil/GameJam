733182286a56fcc5ab018d020ac8e69b1aad0978
KamilChytil <75939657+KamilChytil@users.noreply.github.com>
2024-03-08 22:31:46 +0100

    Enemy
    Patrol,Check PLayer

---
+++ GameJamSpy/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float enemySpeed;
+    public float enemyWait;
+    public Transform[] enemyPoints;
+    private int currentPointIndex = 0;
+    public EnemyShoot enemyShoot;
+
+    private float timeToWait = 0f;
+
+    public bool isEnemyAlive = true;
+
+
+    void Start()
+    {
+        isEnemyAlive = true;
+        enemyShoot = GetComponent<EnemyShoot>();
+        Transform childTransform = transform.GetChild(1);
+        enemyShoot = childTransform.GetComponent<EnemyShoot>();
+    }
+
+    void Update()
+    {
+        if(isEnemyAlive == true)
+        {
+            EnemyMoveToPoint();
+
+        }
+        else 
+        {
+            Debug.Log("EnemyDead");
+
+        }
+    }
+
+
+    private void ChangePoint()
+    {
+        currentPointIndex++;
+
+        if(currentPointIndex == enemyPoints.Length)
+        {
+            currentPointIndex = 0;
+        }
+
+    }
+
+
+    private void PlayerEntrShootArea()
+    {
+        Vector3 targetDirection = enemyShoot.playerLocation.position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 5f);
+        timeToWait += Time.deltaTime;
+        transform.position = transform.position;
+
+    }
+
+    private void RotationEnemy()
+    {        
+        Vector3 targetDirection = enemyPoints[currentPointIndex].position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 5f);
+
+    }
+
+
+    private void EnemyMoveToPoint()
+    {
+
+        if(enemyShoot.isPLayerInArea == true && timeToWait <= 1f)
+        {
+            PlayerEntrShootArea();
+        }
+        else
+        {
+            enemyShoot.isPLayerInArea = false;
+            timeToWait = 0f;
+            RotationEnemy();
+            transform.position = Vector3.MoveTowards(transform.position, enemyPoints[currentPointIndex].position, enemySpeed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, enemyPoints[currentPointIndex].position) < 0.1f)
+            {
+                ChangePoint();
+            }
+
+        }
+
+
+    }
+
+}
