45cd162ff64193b7cd4f08f296fd415f572f0e15
Matouš Marek <mylapqn312@gmail.com>
2024-03-09 00:55:37 +0100

    idk

--- GameJamSpy/Assets/Scripts/PlayerMovement.cs
+++ GameJamSpy/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 10f;
     CharacterController controller;
     public Transform cameraRoot;
+    public Transform gunFX;
+    float timeSinceGunshot = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +21,22 @@ public class PlayerMovement : MonoBehaviour
         Ray r = Camera.main.ScreenPointToRay(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
         if (Physics.Raycast(r, out RaycastHit hitInfo, 100, LayerMask.GetMask("Ground")))
         {
-            transform.rotation = Quaternion.LookRotation(hitInfo.point - transform.position,Vector3.up);
+            transform.rotation = Quaternion.LookRotation(hitInfo.point - transform.position, Vector3.up);
         }
         controller.Move(new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * speed * Time.deltaTime);
-        cameraRoot.position = Vector3.Lerp(cameraRoot.position,transform.position,Time.deltaTime*5);
+        cameraRoot.position = Vector3.Lerp(cameraRoot.position, transform.position, Time.deltaTime * 5);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            gunFX.gameObject.SetActive(true);
+            timeSinceGunshot=0;
+        }
+        else
+        {
+            timeSinceGunshot += Time.deltaTime;
+            if (timeSinceGunshot >= .1)
+                gunFX.gameObject.SetActive(false);
+        }
 
     }
 }
