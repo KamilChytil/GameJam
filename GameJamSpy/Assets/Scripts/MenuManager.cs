5c360140e6666385f6a28421ad7bf1932e67a1a6
KamilChytil <75939657+KamilChytil@users.noreply.github.com>
2024-03-10 03:47:22 +0100

    Menu

---
+++ GameJamSpy/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+
+    private GameObject menuScreen;
+    private GameObject selectLevelScreen;
+    private GameObject settingScreen;
+
+    public Toggle musicToggle;
+    public Toggle soundToggle;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuScreen = GameObject.Find("Menu");
+        selectLevelScreen = GameObject.Find("SelectLevelScreen");
+        settingScreen = GameObject.Find("SettingScreen");
+
+        musicToggle.isOn = PlayerPrefs.GetInt("MusicEnabled", 1) == 1;
+        soundToggle.isOn = PlayerPrefs.GetInt("SoundEnabled", 1) == 1;
+
+
+        menuScreen.SetActive(true);
+        settingScreen.SetActive(false);
+        selectLevelScreen.SetActive(false);
+        SetMusicEnabled(musicToggle.isOn);
+        SetSoundEnabled(soundToggle.isOn);
+    }
+
+
+
+
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+    public void ToggleMusic(bool isEnabled)
+    {
+        SetMusicEnabled(isEnabled);
+        PlayerPrefs.SetInt("MusicEnabled", isEnabled ? 1 : 0);
+    }
+
+    public void ToggleSound(bool isEnabled)
+    {
+        SetSoundEnabled(isEnabled);
+        PlayerPrefs.SetInt("SoundEnabled", isEnabled ? 1 : 0);
+    }
+
+    private void SetMusicEnabled(bool isEnabled)
+    {
+        // AudioListener.pause = !isEnabled;
+    }
+
+    private void SetSoundEnabled(bool isEnabled)
+    {
+
+        //AudioManager.instance.SetSoundEnabled(isEnabled);
+    }
+    public void BackButton()
+    {
+        menuScreen.SetActive(true);
+        settingScreen.SetActive(false);
+        selectLevelScreen.SetActive(false);
+
+
+    }
+
+
+    public void OpenSettingScreen()
+    {
+        settingScreen.SetActive(true);
+        menuScreen.SetActive(false);
+        selectLevelScreen.SetActive(false);
+    }
+
+    public void OpenLevelScreen()
+    {
+        settingScreen.SetActive(false);
+        menuScreen.SetActive(false);
+        selectLevelScreen.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void LoadLevelById(string levelName)
+    {
+        switch(levelName)
+        {
+            case "Level1":
+                SceneManager.LoadScene(levelName);
+                break;
+            case "Level2":
+                SceneManager.LoadScene(levelName);
+                break;
+            case "Level3":
+                SceneManager.LoadScene(levelName);
+                break;
+
+
+        }
+
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
